2903199445f4f9bda095a722c39383f0d11b87f8
zishenyang <yangzishen@139.com>
2021-03-27 19:46:34 +0800

    RegisterrController
    RegisterrController

---
+++ meet2/src/main/java/com/example/meet/controller/RegisterrController.java
@@ -0,0 +1,16 @@
+package com.example.meet.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName RegisterrController
+ * @Author LeeChang
+ * @Date 2021/3/27 19:44
+ * @Description RegisterrController
+ * @Version 1.0
+ */
+ @Controller
+
+public class RegisterrController {
+
+}
