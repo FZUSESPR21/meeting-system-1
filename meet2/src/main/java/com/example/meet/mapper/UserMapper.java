bbf013087a1533266919eee0d53ca5e1f1d1e4d5
Leizhenkang <54850663+Leizhenkang@users.noreply.github.com>
2021-03-27 16:32:33 +0800

    增加注册功能

--- meet2/src/main/java/com/example/meet/mapper/UserMapper.java
+++ meet2/src/main/java/com/example/meet/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.example.meet.mapper;
 
 import com.example.meet.bean.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -16,4 +18,9 @@ public interface UserMapper {
 
     @Select("SELECT * FROM user")
     public List<User> getAllUser();
+
+    @Insert("INSERT INTO user VALUES(#{userid},#{name},#{password},#{meetingid})")
+    public Integer registerUser(@Param("userid") String userid, @Param("name") String name,
+                                @Param("password") String password, @Param("meetingid") Integer meetingid);
+
 }
