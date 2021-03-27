b6b9f63ee77d88ae581b0b07e44fd41582a444be
hzj2870757140 <https://github.com/hzj2870757140>
2021-03-27 16:58:10 +0800

    实现了各种身份的登录

--- meet2/src/main/resources/static/js/login.js
+++ meet2/src/main/resources/static/js/login.js
@@ -1,9 +1,12 @@
 var Main = {
     data() {
       var validatePass = (rule, value, callback) => {
+        var zg =  /^[0-9a-zA-Z]*$/;
         if (value === '') {
           callback(new Error('请输入账号'));
-        } else {
+        } else if(!zg.test(value)){
+          callback(new Error('账号只能为字母和数字'));
+        }else {
           if (this.ruleForm.checkPass !== '') {
             this.$refs.ruleForm.validateField('checkPass');
           }
@@ -11,9 +14,12 @@ var Main = {
         }
       };
       var validatePass2 = (rule, value, callback) => {
+        var zg =  /^[0-9a-zA-Z]*$/;
         if (value === '') {
           callback(new Error('请输入密码'));
-        } else {
+        } else if(!zg.test(value)){
+          callback(new Error('密码只能为字母和数字'));
+        }else {
           callback();
         }
       };
@@ -35,6 +41,25 @@ var Main = {
     },
     methods: {
       submitForm(formName) {
+        //普通人的登录
+        var password=this.ruleForm.checkPass;
+        var name=this.ruleForm.pass;
+        var k=name+"+"+password;
+        $.ajax(
+          {
+              url: '',
+              type: 'get',
+              data: k,
+              }).then(function (res) 
+              {
+                alert(1);　　　　
+              }).fail(function () 
+              {
+                  console.log('失败');
+              })
+      },
+      submitForm1(formName) {
+        //主席 秘书啥的的登录
         var password=this.ruleForm.checkPass;
         var name=this.ruleForm.pass;
         var k=name+"+"+password;
