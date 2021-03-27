ec3e33031a8a0db152115a285f0af2cff38cc734
hb594709591 <594709591@qq.com>
2021-03-27 16:41:45 +0800

    添加js中的ajax提供给后端获取json数据
    通过后端的url获取用户信息。然后传给秘书界面的table展示给秘书来收集用户信息

--- meet2/src/main/resources/static/js/secretaryinterface.js
+++ meet2/src/main/resources/static/js/secretaryinterface.js
@@ -1,13 +1,33 @@
+var tableData = [];
+window.onload = function(){
+    $.ajax(
+        {
+            url: '',
+            type: 'get',
+            data: {},
+            dataType: 'json'
+        }).then(function (res)
+    {
+        for (var i in res)
+        {
+            tableData.push(res[i])  //把从json获取的数据赋值给数组
+        }
+    }).fail(function ()
+    {
+        console.log('失败');
+    })
+}
+
 var Main = {
     data() {
       return {
-        tableData: [{
-        userid: '账号',
-          name: '王小虎',
-          meetingid: '分论坛1'
-        }]
+        tableData
       }
     }
+  }
+
+  function turn(){
+      window.open('test.html');
   }
 var Ctor = Vue.extend(Main)
 new Ctor().$mount('#secretary')
