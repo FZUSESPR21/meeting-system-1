ec665680a067dfe6de1cd47996d96c35743b8e39
zishenyang <yangzishen@139.com>
2021-03-27 22:51:38 +0800

    db
    db

---
+++ message.sql
@@ -0,0 +1,5 @@
+INSERT INTO `message`(`msgid`, `msgdata`, `adminname`, `meetingid`, `time`) VALUES (1, '分主席1在1分会', '分主席1', 1, NULL);
+INSERT INTO `message`(`msgid`, `msgdata`, `adminname`, `meetingid`, `time`) VALUES (2, '分秘书1在1分会', '分会秘书1', 1, NULL);
+INSERT INTO `message`(`msgid`, `msgdata`, `adminname`, `meetingid`, `time`) VALUES (3, '分主席2在2分会', '分主席2', 2, NULL);
+INSERT INTO `message`(`msgid`, `msgdata`, `adminname`, `meetingid`, `time`) VALUES (4, '分秘书2在2分会', '分会秘书2', 2, NULL);
+INSERT INTO `message`(`msgid`, `msgdata`, `adminname`, `meetingid`, `time`) VALUES (5, '你好', '分主席', 1, '2021-03-27 21:26:59');
