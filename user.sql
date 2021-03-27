63b9d977ce661158116936bef9891dbef58f48f8
koopi0123 <1034088383@qq.com>
2021-03-27 22:47:46 +0800

    Create user.sql
    用户表

---
+++ user.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user`(`userid`, `name`, `password`, `meetingid`) VALUES ('1', '关注分会1小明', '123', 1);
+INSERT INTO `user`(`userid`, `name`, `password`, `meetingid`) VALUES ('2', '关注分会2小王', '123', 2);
+INSERT INTO `user`(`userid`, `name`, `password`, `meetingid`) VALUES ('3', '关注分会1、2小红', '123', 3);
+INSERT INTO `user`(`userid`, `name`, `password`, `meetingid`) VALUES ('4', '小黑', '666', 1);
