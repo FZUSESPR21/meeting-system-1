8bdef11ad7063a5b3d44c8dced2fa0254da8f940
koopi0123 <1034088383@qq.com>
2021-03-27 22:47:37 +0800

    管理员表

---
+++ admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO `admin`(`adminid`, `name`, `password`, `identity`) VALUES ('00', '主席', '111', 1);
+INSERT INTO `admin`(`adminid`, `name`, `password`, `identity`) VALUES ('11', '分主席1', '111', 2);
+INSERT INTO `admin`(`adminid`, `name`, `password`, `identity`) VALUES ('12', '分会秘书1', '111', 3);
+INSERT INTO `admin`(`adminid`, `name`, `password`, `identity`) VALUES ('21', '分主席2', '111', 4);
+INSERT INTO `admin`(`adminid`, `name`, `password`, `identity`) VALUES ('22', '分会秘书2', '111', 5);
