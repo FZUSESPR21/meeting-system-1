c1f5c2173e02e91a89b089f2795890abe783aac6
csk <1649537125@qq.com>
2021-03-27 10:33:40 +0800

    搭建springboot目录结构

---
+++ src/main/java/com/example/meetingsystem1/MeetingSystem1Application.java
@@ -0,0 +1,13 @@
+package com.example.meetingsystem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSystem1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingSystem1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meetingsystem1/MeetingSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingsystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSystem1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
