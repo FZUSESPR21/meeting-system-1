058d2984c69a5df971d152733aeaa8af257e0900
Leizhenkang <54850663+Leizhenkang@users.noreply.github.com>
2021-03-27 17:11:45 +0800

    前后端修改服务器问题

--- meet2/src/main/java/com/example/meet/controller/TestController.java
+++ meet2/src/main/java/com/example/meet/controller/TestController.java
@@ -5,6 +5,7 @@ import com.example.meet.bean.UserShow;
 import com.example.meet.service.serviceImp.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,8 +22,10 @@ public class TestController {
     @Autowired
     UserServiceImp userServiceImp;
 
+
     @ResponseBody
     @GetMapping("/get")
+    @CrossOrigin(origins = "*", maxAge = 3600)
     public List<UserShow> getUser(){
         List<UserShow> list = userServiceImp.getAllUser();
         return list;
